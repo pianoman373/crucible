22cd97c67621eac2d62a31807a5f56920c8a2cd8
pianoman373 <jholley373@yahoo.com>
2019-03-07 18:52:46 -0600

    Change sprite rendering to be top left centered.

--- src/Primitives.cpp
+++ src/Primitives.cpp
@@ -125,15 +125,15 @@ void Primitives::sphere(Mesh &m, unsigned int xSegments, unsigned int ySegments)
 
 void Primitives::sprite(Mesh &m) {
 	m.positions = std::vector<vec3>{
-	    { 0.5f,  0.5f, 0.0f },
-		{ 0.5f, -0.5f, 0.0f },
-        { -0.5f, -0.5f, 0.0f },
+	    { 1.0f,  1.0f, 0.0f },
+		{ 1.0f, 0.0f, 0.0f },
+        { 0.0f, 0.0f, 0.0f },
 
 
 
-        { -0.5f, -0.5f, 0.0f },
-		{ -0.5f,  0.5f, 0.0f },
-		{ 0.5f,  0.5f, 0.0f },
+        { 0.0f, 0.0f, 0.0f },
+		{ 0.0f,  1.0f, 0.0f },
+		{ 1.0f,  1.0f, 0.0f },
 	};
 	m.uvs = std::vector<vec2>{
 	    { 1.0f, 1.0f },
